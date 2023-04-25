7611974043a81d0414783a36d1ab5f84e3077734
maede kaboli <kabolimaede@gmail.com>
2023-04-25 14:55:05 +0330

    about header responsive is done

--- src/views/about/AboutUsHeader.vue
+++ src/views/about/AboutUsHeader.vue
@@ -2,9 +2,9 @@
     <div class="AboutUsHeader">
         <v-container>
             <v-row>
-                <div class="title-and-btn">
+                <div class="title-and-btn px-3">
                     <h1 class="mb-13">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</h1>
-                    <v-btn size="large" color="#3B5099" class="text-white">ورود</v-btn>
+                    <v-btn width="252" height="62" color="#3B5099" class="text-white">ورود</v-btn>
                 </div>
                 <div class="images">
                     <v-img src="/mosalas.svg"></v-img>
@@ -23,7 +23,14 @@
 
     .title-and-btn {
         h1 {
+            position: relative;
+            z-index: 1;
             width: 590px;
+        }
+
+        .v-btn {
+            position: relative;
+            z-index: 1;
         }
     }
 
@@ -48,6 +55,45 @@
 
         .v-img {
             width: 100px;
+        }
+    }
+
+    @media only screen and (max-width:768px) {
+        .images[data-v-0ac1398f] {
+            left: 0px;
+            top: 50px;
+            transform: unset;
+        }
+
+        .title-and-btn {
+            h1 {
+                width: 410px;
+                font-size: 24px;
+                line-height: 37px;
+            }
+        }
+    }
+
+    @media only screen and (max-width:360px) {
+        .images {
+            left: -24px !important;
+            top: 25px !important;
+            transform: scale(0.55) !important;
+        }
+
+        .title-and-btn {
+            h1 {
+                width: 100%;
+                font-size: 16px;
+                line-height: 25px;
+                text-align: center;
+                margin-bottom: 46px !important;
+            }
+
+            .v-btn {
+                width: 100% !important;
+                height: 48px !important;
+            }
         }
     }
 }
