09022f4c9f8a04e4fe2174f96de36dc4bee0c897
maede kaboli <kabolimaede@gmail.com>
2023-04-25 19:31:46 +0330

    home clients section responsive is done

--- src/views/home/OurClients.vue
+++ src/views/home/OurClients.vue
@@ -12,10 +12,10 @@ const items = ref([
     <div class="OurClients">
         <v-container class="py-0">
             <h2>مشتریان ما</h2>
-            <v-row>
-                <v-col class="d-flex flex-wrap justify-center" v-for="(item, i) in items" :key="i" cols="md-4">
+            <v-row class="justify-center">
+                <v-col class="d-flex flex-wrap justify-center" v-for="(item, i) in items" :key="i" cols="md-4" sm="6">
                     <v-sheet class="mb-4 OurClients-img">
-                        <v-img cover src="/circle-pointer.svg" class="circle-question"></v-img>
+                        <v-img width="68" height="68" cover src="/blog1.svg" class="circle-question"></v-img>
                     </v-sheet>
                     <v-sheet>
                         {{ item.text }}
@@ -43,13 +43,40 @@ const items = ref([
         direction: rtl;
         border-radius: 16px;
         padding: 24px;
+
+        .v-img {
+            margin: auto;
+        }
     }
 
     .OurClients-img {
         box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
         border-radius: 68px;
-        height: 122px;
+        padding: 27px 0;
         width: 297px;
+    }
+
+    @media only screen and (max-width:360px) {
+        h2 {
+            font-size: 16px !important;
+            margin-bottom: 24px;
+        }
+    }
+
+    @media only screen and (max-width:768px) {
+
+        .OurClients-img {
+            width: 201px;
+            padding: 20px 0;
+        }
+
+        .v-sheet {
+
+            .v-img {
+                width: 34px !important;
+                height: 34px !important;
+            }
+        }
     }
 }
 </style>
