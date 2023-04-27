d5a249c17f309841ec149d45813af7a67e579e56
maede kaboli <kabolimaede@gmail.com>
2023-04-27 13:45:10 +0330

    home header title is responsive

--- src/views/home/HomeHeader.vue
+++ src/views/home/HomeHeader.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="TheHeader">
-        <div class="TheHeader-title">ADAKMARIN</div>
+        <v-container class="pa-0">
+            <div class="TheHeader-title">ADAKMARIN</div>
+        </v-container>
     </div>
 </template>
 
@@ -15,10 +17,50 @@
         justify-content: center;
         font-style: normal;
         font-weight: 700;
-        font-size: 140.019px;
-        line-height: 82px;
+        font-size: 122px;
+        line-height: 129px;
         letter-spacing: 0.4em;
         color: #122771;
+    }
+
+    @media only screen and (max-width:1280px) {
+
+        .TheHeader-title {
+            font-size: 100px;
+            line-height: 124px;
+        }
+
+    }
+
+    @media only screen and (max-width:960px) {
+
+        .TheHeader-title {
+            font-size: 80px;
+            line-height: 112px;
+        }
+
+    }
+
+    @media only screen and (max-width:768px) {
+        & {
+            height: 56px;
+
+            .TheHeader-title {
+                font-size: 50px;
+                line-height: 79px;
+            }
+        }
+    }
+
+    @media only screen and (max-width:360px) {
+        & {
+            height: 38px;
+
+            .TheHeader-title {
+                font-size: 30px;
+                line-height: 53px;
+            }
+        }
     }
 }
 </style>
