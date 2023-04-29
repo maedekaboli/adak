e0cb874f03a2344cee0aac6c0c56957e87203709
maede kaboli <kabolimaede@gmail.com>
2023-04-29 12:34:29 +0330

    404 page added

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,15 +5,19 @@ const Blog = () => import('../views/blog/BlogContainer.vue')
 const ContactUs = () => import('../views/contact/ContactUs.vue')
 const AboutUs = () => import('../views/about/AboutUsContainer.vue')
 const CreateProduct = () => import('../views/product/CreateProduct.vue')
+const SingleBlog = () => import('../views/blog/SingleBlog.vue')
+const NotFound = () => import('../views/notFound/NotFound.vue')
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', name: 'home', component: Home },
-        { path: '/blog', name: 'blog', component: Blog },
+        { path: '/blogs', name: 'blog', component: Blog },
+        { path: '/blogs/:id', name: 'singleBlog', component: SingleBlog },
         { path: '/contactUs', name: 'contactUs', component: ContactUs },
         { path: '/aboutUs', name: 'aboutUs', component: AboutUs },
-        { path: '/newProduct', name: 'newProduct', component: CreateProduct }
+        { path: '/newProduct', name: 'newProduct', component: CreateProduct },
+        { path: '/:pathMatch(.*)*', name: 'notFound', component: NotFound },
     ]
 })
 export default router
---
+++ src/views/notFound/NotFound.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="NotFound">
+        <v-container>
+            <v-row class="justify-center">
+                <v-col cols="sm-8">
+                    <v-img class="ma-auto" width="500" src="/404.jpg"></v-img>
+                    <h2>صفحه مورد نظر پیدا نشد</h2>
+                    <RouterLink to="/">
+                        <v-btn color="#3B5099" height="62" width="150" class="text-white">صفحه اصلی</v-btn>
+                    </RouterLink>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<style lang="scss">
+.NotFound {
+    direction: rtl;
+    text-align: center;
+    margin-bottom: 24px;
+
+    h2 {
+        font-weight: 700;
+        font-size: 24px;
+        line-height: 37px;
+        margin-bottom: 24px;
+        color: #181818;
+    }
+
+    @media only screen and (max-width:360px) {
+        .v-img {
+            width: 100% !important;
+        }
+
+        h2 {
+            font-size: 14px;
+            line-height: 22px;
+        }
+
+        .v-btn {
+            height: 48px !important;
+        }
+    }
+}
+</style>
